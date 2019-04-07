a8b4adeb7a81f3800fc34d51424168e238bb6e60
Anthony Baker <bakera81@icloud.com>
2019-04-07 21:17:49 +0200

    things are getting weird

--- itunesutils/R/hello.R
+++ itunesutils/R/hello.R
@@ -17,17 +17,21 @@
 library(xml2)
 library(rvest)
 library(tidyverse)
+library(purrr)
+library(tictoc)
 
-lib <- read_xml("../Library.xml")
+lib <- read_xml("~/Documents/itunesutils/data/Library.xml")
+lib <- read_xml("~/Documents/libpytunes/libpytunes/tests/Test Library.xml")
 
 playlists_root <- lib %>%
   xml_nodes(xpath = "//key[text() = 'Playlists']/following-sibling::array")
 
-dict <- playlists_root %>%
-  xml_child(search = 13)
-  # as_list()
+# dict <- playlists_root %>%
+#   xml_child(search = 13)
 
-parse_dict <- function(dict) {
+# This works but is god-awful slow
+dict_to_list <- function(dict) {
+  # profvis::profvis({
   playlist <- list()
   for (node in dict %>% xml_children()) {
     if (node %>% xml_name() == "key") {
@@ -37,65 +41,159 @@ parse_dict <- function(dict) {
         value <- c()
         for (n in next_sibling %>% xml_children()) {
           # browser()
-          value <- c(value, parse_dict(n))
+          value <- c(value, dict_to_list(n))
         }
       } else {
         value <- next_sibling %>% xml_text()
       }
-      print(paste0(key, ": ", value))
+      # print(paste0(key, ": ", value))
+      if (key == "Name" & value %in% c("Library", "Music")) {
+        return(NA)
+      }
+      if (key == "Name") {
+        print(paste0("Parsing ", value, "..."))
+      }
       playlist[[key]] <- value
     }
   }
   playlist
+  # })
 }
 
-p <- parse_dict(dict)
+# Experimental version of above
+# dict_to_list <- function(dict) {
+  # profvis::profvis({
+  playlist <- list()
 
-as.data.frame(p) %>%
-  tibble() %>%
-  View()
+  dict <- playlists_root %>%
+    xml_child()
 
-    # xpath <- paste0("/key[text() = '", key, "']/following-sibling")
-    # dict %>% xml_children() %>% xml_nodes(xpath = xpath)
+  l <- dict %>%
+    xml_children() %>%
+    as_list()
 
-    # next_sibling <- (node %>% xml_siblings())[[1]]
-    # next_sibling %>% xml_remove()
-    # node %>% xml_remove()
-
-
-
-    # list(node %>% xml_text() = node %>% xml_node(xpath = "following-sibling")
-    # node %>% xml_node(xpath = "following-sibling")
+  keys <- unlist(l[c(T, F)])
+  values <- unlist(l[c(F, T)], recursive = F)
+  values_raw <- l[c(F, T)]
 
 
-# parse_xml <- function(path) {
-  root <- xml2::read_xml(path)
-  top_level <- xml_children(root)
-  top_level <- xml_children(top_level)
+  names(values) <- keys
 
-tracks_index <- which(xml_text(top_level) == "Tracks") + 1
-playlists_index <- which(xml_text(top_level) == "Playlists") + 1
+  # Create a meta df
+  meta <- tibble(
+    keys = keys,
+    values = values_raw,
+    lengths = lengths(values_raw)
+  )
 
-playlists <- xml_children(top_level[playlists_index])
 
-p <- playlists[1]
 
-schema <- list(
-  playlists = c("Name", "Description", "Master", "Playlist ID", "Playlist Persistent ID", "Visible", "All Items", "Playlist Items")
-)
+  expand_playlists <- function(x) {
+    # browser()
+    if (length(x) == 0) {
+      NA_character_
+    } else if (length(x) == 1) {
+      unlist(x)
+    } else {
+      # browser()
+      # expand_track_ids(x)
+      map_chr(x, fetch_int_value)
+    }
+  }
 
-parse_playlist <- function(p) {
-  # names <- xml_text(xml_find_all(p, "key"))
-  # values <- xml_text()
-  # xml_children(p)[xml_name(xml_children(p)) == "key"]
-  names <- xml_text(xml_children(p)[which(xml_name(xml_children(p)) == "key")])
-  values <- xml_children(p)[which(xml_name(xml_children(p)) != "key")]
+  expand_track_ids <- function(x) {
+    # browser()
+    map_chr(x, fetch_int_value)
+    # map_chr(x, ~ .$dict$integer[[1]])
+  }
 
-  names() <- names
-  values
+  fetch_int_value <- function(x) {
+    # browser()
+    # x$integer[[1]]
+    x$integer[[1]]
+  }
+
+  playlist <- meta %>%
+    mutate(values = map(values, expand_playlists)) %>%
+    unnest()
+
+  playlist %>%
+    group_by(keys) %>%
+    nest(values) %>%
+    spread(keys, data) %>%
+    mutate_at(vars(-`Playlist Items`), ~.[[1]]$values)
+
+  meta %>%
+    mutate(v2 = map(values, ~expand_playlists(values, lengths)))
+
+  playlist <- list()
+  for (i in length(keys)) {
+    if ()
+    playlist[[keys[i]]] <- unlist(values_raw[i])
+  }
+
+
+  for (node in dict %>% xml_children()) {
+    if (node %>% xml_name() == "key") {
+      key <- node %>% xml_text()
+      next_sibling <- node %>% xml_node(xpath = "./following-sibling::*")
+      if (next_sibling %>% xml_name() == "array") {
+        value <- c()
+        for (n in next_sibling %>% xml_children()) {
+          # browser()
+          value <- c(value, dict_to_list(n))
+        }
+      } else {
+        value <- next_sibling %>% xml_text()
+      }
+      # print(paste0(key, ": ", value))
+      if (key == "Name" & value %in% c("Library", "Music")) {
+        return(NA)
+      }
+      if (key == "Name") {
+        print(paste0("Parsing ", value, "..."))
+      }
+      playlist[[key]] <- value
+    }
+  }
+  playlist
+  # })
 }
 
 
-doc <- read_xml(path)
-p2 <- xml_ns_strip(playlists)
-doc <- xml_ns_strip()
+
+
+dict_to_df <- function(dict) {
+  l <- dict_to_list(dict)
+
+  as_tibble(l) #%>%
+    # unnest() %>%
+    # nest(`Playlist Items`,
+    #      .key = `Playlist Items`)
+}
+
+p <- dict_to_list(dict)
+
+df <- dict_to_df(dict)
+
+for(i in 1:20) {
+  print(
+    playlists_root %>%
+    xml_child(search = i) %>%
+    xml_child(search = 2) %>%
+    xml_text()
+  )
+}
+
+dict <- playlists_root %>%
+  xml_child(search = 14)
+
+profvis::profvis({
+  dict_to_df(dict)
+})
+
+tic()
+playlists_df <- playlists_root %>%
+  xml_children() %>% head(2) %>%
+  map_dfr(dict_to_df)
+toc()

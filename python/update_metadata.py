485ec8a21f41a13c992a49d5b019b603d30b68ed
Anthony Baker <bakera81@icloud.com>
2019-09-14 18:38:03 -0400

    WIP: applescript methods to update metadata

--- python/update_metadata.py
+++ python/update_metadata.py
@@ -1,37 +1,42 @@
-import pickle
-import pandas as pd
-from datetime import datetime
-from libpytunes import Library
-form time import mktime
-import os
-from siuba import join
 
-base_path = "/Users/AB/Documents/itunesutils"
-old_lib_path = "data/2016-07-15/Library.xml"
-old_pickle_file = "data/2016-07-15/itl.p"
-new_lib_path = "data/2019-07-31/Library.xml"
-new_pickle_file = "data/2019-07-31/itl.p"
+import applescript
 
-itl_source = Library(old_lib_path)
-pickle.dump(itl_source, open(old_pickle_file, "wb"))
+script = applescript.AppleScript("""
+    on updateDateAdded(pId, newDateAdded)
+        tell application "iTunes"
+            set theSong to (the first track whose persistent ID is pId)
+            tell theSong
+                set date added to newDateAdded
+            end tell
+        end tell
+    end updateDateAdded
 
-itl_source = Library(new_lib_path)
-pickle.dump(itl_source, open(new_pickle_file, "wb"))
+    on updatePlaycount(pId, newPlaycount)
+        tell application "iTunes"
+            set theSong to (the first track whose persistent ID is pId)
+            tell theSong
+                set played count to newPlaycount
+            end tell
+        end tell
+    end updatePlaycount
 
-itl = pickle.load(open(old_pickle_file, "rb"))
-songs_data = [s[1].ToDict() for s in itl.songs.items()]
-songs_1 = pd.DataFrame(songs_data)
+    on updateSkipcount(pId, newSkipcount)
+        tell application "iTunes"
+	       set theSong to (the first track whose persistent ID is pId)
+	       tell theSong
+		         set skipped count to newSkipcount
+	       end tell
+        end tell
+    end updateSkipcount
 
-itl = pickle.load(open(new_pickle_file, "rb"))
-songs_data = [s[1].ToDict() for s in itl.songs.items()]
-songs_2 = pd.DataFrame(songs_data)
+    on updateGenre(pId, newGenre)
+        tell application "iTunes"
+	       set theSong to (the first track whose persistent ID is pId)
+	       tell theSong
+		         set genre to newGenre
+	       end tell
+        end tell
+    end updateGenre
+""")
 
-# correct datetimes
-songs_2 = songs_2 >> \
-    mutate(date_added = lambda: _.date_added. datetime.fromtimestamp(mktime(date_added)))
-
-convert_time = lambda x: datetime.fromtimestamp(mktime(x))
-songs_2.date_added.apply(convert_time).astype("datetime64[ns]")
-
-
-summarize(songs_2, max_date_added = _.date_added.max())
+print(script.call('updateGenre', '9650D59787154E54', 'yah bish'))

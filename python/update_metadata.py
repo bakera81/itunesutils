76ce25b1fec782d7d25e861123518de5d7c67c66
Anthony Baker <bakera81@icloud.com>
2019-10-04 20:24:00 +0200

    add more applescript functions

--- python/update_metadata.py
+++ python/update_metadata.py
@@ -48,16 +48,16 @@ script = applescript.AppleScript("""
 """)
 
 # To update the date:
-# 1. Return the path of the file
-# 2. Copy the file to ~/Desktop/itunesutils_tmp
-# 3. Return all metadata for the track (playlists, rating, loved)
-# 4. Delete the song
-#### TODO: does the pId remain the same? we need to be able to update the other metadata too
+# 1. DONE Return the path of the file
+# 2. DONE Copy the file to ~/Desktop/itunesutils_tmp
+# 3. DONE Return all metadata for the track (pId, playlists, rating, loved)
+# 4. DONE Delete the song
 # 5. Get new metadata from spreadsheet
 # 6. Turn back the clock
 # 7. DONE Add the song
 # 8. Add the song to all playlists, update metadata
-date_script = applescript.AppleScript("""
+#### Note: pId does not remain the same. We will need to remap the pID
+helper_script = applescript.AppleScript("""
     on getFilePath(pId)
     	tell application "iTunes"
     		set theTrack to (the first track whose persistent ID is pId)
@@ -73,11 +73,35 @@ date_script = applescript.AppleScript("""
         set theFile to POSIX file path
         tell application "iTunes" to add theFile
     end addFile
+
+    on getMetaData(pId)
+        tell application "iTunes"
+    	   set theTrack to the first track whose persistent ID is pId
+    	   get properties of theTrack
+        end tell
+    end getMetaData
+
+    on deleteTrack(pId)
+        tell application "iTunes"
+            delete (the first track whose persistent ID is "BE03E03F897BBBCE")
+        end tell
+    end deleteTrack
 """)
 
 
-path = date_script.call('getFilePath', '9650D59787154E54')
 
+# Steps 1-2
+path = date_script.call('getFilePath', '9650D59787154E54')
 copy(path, '/Users/AB/Desktop/itunesutils_tmp/')
 
-print(script.call('updateGenre', '9650D59787154E54', 'yah bish'))
+#print(script.call('updateGenre', '9650D59787154E54', 'yah bish'))
+
+# Step 3
+meta_raw = date_script.call('getMetaData', '9650D59787154E54')
+meta = {k.code.decode(): v for k,v in meta_raw.items()}
+
+
+# tell application "iTunes"
+# 	set theTrack to item 1 of (get selection)
+# 	get persistent ID of theTrack
+# end tell
